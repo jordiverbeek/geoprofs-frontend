9685861601e91b6f032514e1c9c756ab1c064053
Jordi Verbeek <jordi.verbeek@gmail.com>
2024-09-25 10:52:35 +0200

    sidebar updated
    added when on Login, Register or Logout the sidebar doesn't show

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Sidebar from './components/Sidebar'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [showSidebar, setShowSidebar] = useState(true);
+
+    useEffect(() => {
+        const url = window.location.pathname;
+        let regex = /^\/auth\/(login|register|logout)$/;
+    
+        if (regex.test(url)) {
+          setShowSidebar(false);
+        } else {
+          setShowSidebar(true);
+        }
+      }, [location]);
+    
+    
 
     return (
         <section className='vlx-main-page'>
             <BrowserRouter>
-                <Sidebar />
+                {
+                    showSidebar ? <Sidebar /> : null
+                }
+
                 <section className='vlx-body'>
                     <Routes>
                         <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
+                        <Route path="/auth/login" element={<Login />} />
                     </Routes>
                 </section>
             </BrowserRouter>
