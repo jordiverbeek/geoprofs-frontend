78fa6570238bc35436a321c9db56b5ac8e34945b
Jordi Verbeek <jordi.verbeek@gmail.com>
2024-09-26 15:47:23 +0200

    login bijna af
    moet alleen nog testen hoe ik de API-token terug krijg en moet opvangen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Login = () => {
-  return (
-    <section>
-        <div>
-            <h1>Login</h1>
-            <p>Welcome to the Login</p>
-        </div>
-    </section>
-  )
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
+
+    const sendForm = () => {
+        if (password.length < 8) {
+            setError('Password must be at least 8 characters long')
+            return;
+        } else {
+            const data = {
+                email: email,
+                password: password
+            }
+
+            axios.post('https://geoprofs-backend.test/api/auth/login', data)
+                .then(response => {
+                    console.log(response)
+                    setMessage(response)
+                })
+                .catch(error => {
+                    console.error('Error:', error)
+                    if (error.response.status === 401) {
+                        setError('Email or password is incorrect')
+                        return;
+                    }
+                })
+        }
+    }
+
+    useEffect(() => {
+        setError('');
+    }, [])
+
+    return (
+        <section className='vlx-login'>
+            <div className='container'>
+                <div className='card'>
+                    <h1>LOGIN</h1>
+                    <div className='error-message'>
+                        {error}
+                    </div>
+                    <div className='email-container'>
+                        <label>Email</label>
+                        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className='pw-container'>
+                        <label>Password</label>
+                        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button type="submit" onClick={sendForm}>Login</button>
+                    <Link to={""}>
+                        Wachtwoord vergeten?
+                    </Link>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Login 
