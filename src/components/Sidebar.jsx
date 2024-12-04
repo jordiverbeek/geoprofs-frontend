917a101279602eaca3b83944163d76de117b7d9b
Jordi Verbeek <jordi.verbeek@gmail.com>
2024-12-05 00:41:34 +0100

    design of the verlof aanvraag form done
    design is done only need to add the connection to the back-end

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,23 +2,44 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendarDays, faUserGroup, faGear } from '@fortawesome/free-solid-svg-icons';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import enGB from 'date-fns/locale/en-GB';
+
 
 const Sidebar = () => {
     const [active, setActive] = useState('');
     const navigate = useNavigate();
     const [isModalOpen, setModalOpen] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [selectedReason, setSelectedReason] = useState('');
+    const [selectedButton, setSelectedButton] = useState(null);
+    const [customReason, setCustomReason] = useState('');
 
-    const handleActive = (buttonName) => {
-        if (active === buttonName) {
-            setActive('');
-            return;
-        } else {
-            setActive(buttonName);
-        }
+
+    const handleClick = (button) => {
+        setSelectedButton(button);
     };
 
     const toggleModal = () => {
         setModalOpen(!isModalOpen);
+    };
+
+    const closeModal = (e) => {
+        if (e.target.className === 'modal-overlay') {
+            setModalOpen(false);
+        }
+    };
+
+    const handleReasonChange = (e) => {
+        setSelectedReason(e.target.value);
+        if (e.target.value !== "Overig") {
+            setCustomReason('');
+        }
+    };
+
+    const handleCustomReasonChange = (e) => {
+        setCustomReason(e.target.value);
     };
 
     return (
@@ -39,19 +60,93 @@ const Sidebar = () => {
                             <FontAwesomeIcon className='icons' icon={faCalendarDays} />
                             Planning
                         </Link>
-                        <Link className={active === 'verlof' ? 'active' : ''} onClick={() => { handleActive('verlof'); toggleModal(); }}>
+                        <Link onClick={() => { toggleModal(); }}>
                             <FontAwesomeIcon className='icons' icon={faCalendarDays} />
                             Verlof aanvragen
                         </Link>
                     </div>
                     <div className='container-bot'>
-                        <Link to={'/settings'} className={active === 'settings' ? 'active' : ''} onClick={() => handleActive('settings')}>
+                        <Link to={'/settings'} onClick={() => handleActive('settings')}>
                             <FontAwesomeIcon className='icons' icon={faGear} />
                             Settings
                         </Link>
                     </div>
                 </div>
             </section>
+            {isModalOpen && (
+                <div className="modal-overlay" onClick={closeModal}>
+                    <div className="modal-content">
+                        <h2>Verlof aanvragen</h2>
+                        <div className="body">
+                            <h3>Datum</h3>
+                            <DatePicker
+                                selected={selectedDate}
+                                onChange={(date) => setSelectedDate(date)}
+                                dateFormat="dd/MM/yyyy"
+                                placeholderText="Selecteer een datum"
+                                locale={enGB}
+                            />
+
+                            <h3>Tijd</h3>
+                            <div className="buttons-tijd-container">
+                                <button
+                                    className={selectedButton === 'Ochtend' ? 'selected' : ''}
+                                    onClick={() => handleClick('Ochtend')}
+                                >
+                                    Ochtend
+                                </button>
+                                <button
+                                    className={selectedButton === 'Middag' ? 'selected' : ''}
+                                    onClick={() => handleClick('Middag')}
+                                >
+                                    Middag
+                                </button>
+                                <button
+                                    className={selectedButton === 'Hele dag' ? 'selected' : ''}
+                                    onClick={() => handleClick('Hele dag')}
+                                >
+                                    Hele dag
+                                </button>
+                            </div>
+                            <h3>Verlof reden</h3>
+                            <select
+                                id="verlof-reden"
+                                value={selectedReason}
+                                onChange={handleReasonChange}
+                            >
+                                <option value="" disabled>
+                                    Kies een reden
+                                </option>
+                                <option value="Vakantie">Vakantie</option>
+                                <option value="Ziekte">Ziekte</option>
+                                <option value="Persoonlijk">Persoonlijk</option>
+                                <option value="Ziekte">Zwangerschap</option>
+                                <option value="Persoonlijk">Ouderschapsverlof</option>
+                                <option value="Overig">Overig</option>
+                            </select>
+
+                            {/* Show text input when "Overig" is selected */}
+                            {selectedReason === 'Overig' && (
+                                <>
+                                    <label htmlFor="custom-reason">Specificeer uw reden:</label>
+                                    <input
+                                        type="text"
+                                        id="custom-reason"
+                                        value={customReason}
+                                        onChange={handleCustomReasonChange}
+                                        placeholder="Uw reden..."
+                                    />
+                                </>
+                            )}
+
+                        </div>
+                        <div className="modal-bottom">
+                            <button onClick={toggleModal}>Annuleren</button>
+                            <button onClick={toggleModal}>Bevestigen</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
