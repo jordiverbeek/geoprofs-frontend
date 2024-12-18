b23e00dee362b16e5a952ed55673734610b01144
nick <nickswanenberg@gmail.com>
2024-12-18 14:27:02 +0100

    werknemer toevoegen Modal

--- src/pages/Werknemers.jsx
+++ src/pages/Werknemers.jsx
@@ -1,32 +1,25 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';   
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import Register from './Register'; // Zorg ervoor dat de Register component beschikbaar is
 
-// const url1 = new URL("https://geoprofs-backend.test/api/user");
 const url = new URL("https://geoprofs-backend.vacso.cloud/api/users");
 
 const headers = {
     "Authorization": "Bearer f4bQSBpfssoJEsvhdzqyI4VYQGCLI8gTcKhkbhJo297ef086",
     "Content-Type": "application/json",
     "Accept": "application/json",
-}
+};
 
 fetch(url, {
-    Method: "GET",
+    method: "GET",
     headers,
-}).then(respones => respones.json()).then(data => console.log(data));
+}).then(response => response.json()).then(data => console.log(data));
 
 const werknemers = [
     { naam: "Nick Verbeek", email: "nick@geoprofs.com", groep: "GeoICT > Ontwikkeling" },
     { naam: "Finn Swanenberg", email: "Finn@geoprofs.com", groep: "GeoICT > Scanning" },
     { naam: "Finn Swanenberg", email: "Finn@geoprofs.com", groep: "GeoICT > Testing" },
-    { naam: "Nick Verbeek", email: "nick@geoprofs.com", groep: "GeoICT > Ontwikkeling" },
-    { naam: "Finn Swanenberg", email: "Finn@geoprofs.com", groep: "GeoICT > Scanning" },
-    { naam: "Finn Swanenberg", email: "Finn@geoprofs.com", groep: "GeoICT > Testing" },
-    { naam: "Nick Verbeek", email: "nick@geoprofs.com", groep: "GeoICT > Ontwikkeling" },
-    { naam: "Finn Swanenberg", email: "Finn@geoprofs.com", groep: "GeoICT > Scanning" },
-    { naam: "Finn Swanenberg", email: "Finn@geoprofs.com", groep: "GeoICT > Testing" },
-
 ];
 
 const WerknemerKaart = ({ naam, email }) => (
@@ -51,17 +44,18 @@ const WerknemerGroep = ({ groepNaam, werknemers }) => (
         </div>
         <div className="werknemer-lijst">
             {werknemers.map((werknemer, index) => (
-                <WerknemerKaart
-                    key={index}
-                    naam={werknemer.naam}
-                    email={werknemer.email}
-                />
+                <WerknemerKaart key={index} naam={werknemer.naam} email={werknemer.email} />
             ))}
         </div>
     </div>
 );
 
 const Werknemers = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleModalOpen = () => setIsModalOpen(true);
+    const handleModalClose = () => setIsModalOpen(false);
+
     const gegroepeerdeWerknemers = werknemers.reduce((groepen, werknemer) => {
         if (!groepen[werknemer.groep]) {
             groepen[werknemer.groep] = [];
@@ -74,18 +68,25 @@ const Werknemers = () => {
         <section className='werknemers'>
             <header className="header">
                 <h1 className='werknemers-header'>Werknemers</h1>
-                <button className="toevoegen-knop">
+                <button className="toevoegen-knop" onClick={handleModalOpen}>
                     <FontAwesomeIcon className='icon-plus' icon={faPlus} />
                     Werknemer toevoegen
                 </button>
             </header>
+
             {Object.entries(gegroepeerdeWerknemers).map(([groepNaam, groepWerknemers], index) => (
-                <WerknemerGroep
-                    key={index}
-                    groepNaam={groepNaam}
-                    werknemers={groepWerknemers}
-                />
+                <WerknemerGroep key={index} groepNaam={groepNaam} werknemers={groepWerknemers} />
             ))}
+
+            {/* Modal voor Register Formulier */}
+            {isModalOpen && (
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <button onClick={handleModalClose} className="close-button">Sluiten</button>
+                        <Register />
+                    </div>
+                </div>
+            )}
         </section>
     );
 };
