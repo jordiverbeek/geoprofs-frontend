032e1d84b85df1e0085c67804ff5a44a614b9b46
Jordi Verbeek <jordi.verbeek@gmail.com>
2024-12-18 14:22:59 +0100

    added logout in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarDays, faUserGroup, faGear } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays, faUserGroup, faSignOut } from '@fortawesome/free-solid-svg-icons';
+import Cookies from 'js-cookie';
+
 
 const Sidebar = () => {
     const [active, setActive] = useState('');
     const navigate = useNavigate();
     const [isModalOpen, setModalOpen] = useState(false);
+
+    const handleLogout = () => {
+        Cookies.remove('bearer_token', { path: '' }) 
+        if (Cookies.get('bearer_token') === undefined) {
+            console.log('Cookie removed');
+            console.log('logging out');
+        }
+    };
 
     const handleActive = (buttonName) => {
         if (active === buttonName) {
@@ -27,27 +37,27 @@ const Sidebar = () => {
                 <div className='container'>
                     <div className='container-top'>
                         <h1 onClick={() => navigate('/')}>Verlof Systeem</h1>
-                        <Link to={'/'} className={active === 'dashboard' ? 'active' : ''} onClick={() => handleActive('dashboard')}>
+                        <Link to={'/'} onClick={() => handleActive('dashboard')}>
                             <FontAwesomeIcon className='icons' icon={faCalendarDays} />
                             Dashboard
                         </Link>
-                        <Link to={'/werknemers'} className={active === 'werknemers' ? 'active' : ''} onClick={() => handleActive('werknemers')}>
+                        <Link to={'/werknemers'} onClick={() => handleActive('werknemers')}>
                             <FontAwesomeIcon className='icons' icon={faUserGroup} />
                             Werknemers
                         </Link>
-                        <Link to={'/planning'} className={active === 'planning' ? 'active' : ''} onClick={() => handleActive('planning')}>
+                        <Link to={'/planning'} onClick={() => handleActive('planning')}>
                             <FontAwesomeIcon className='icons' icon={faCalendarDays} />
                             Planning
                         </Link>
-                        <Link className={active === 'verlof' ? 'active' : ''} onClick={() => { handleActive('verlof'); toggleModal(); }}>
+                        <Link onClick={() => { handleActive('verlof'); toggleModal(); }}>
                             <FontAwesomeIcon className='icons' icon={faCalendarDays} />
                             Verlof aanvragen
                         </Link>
                     </div>
                     <div className='container-bot'>
-                        <Link to={'/settings'} className={active === 'settings' ? 'active' : ''} onClick={() => handleActive('settings')}>
-                            <FontAwesomeIcon className='icons' icon={faGear} />
-                            Settings
+                        <Link onClick={() => handleLogout()}>
+                            <FontAwesomeIcon className='icons' icon={faSignOut} />
+                            Logout
                         </Link>
                     </div>
                 </div>
